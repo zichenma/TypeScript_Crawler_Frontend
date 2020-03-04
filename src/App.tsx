b66076dc5839ddc8d2fb3e31d99c4021e0f7af5d
zichenma <zma@liaisionedu.com>
2020-03-04 09:39:56 -0500

    finsihed 8_4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LoginPage from './Pages/Login';
+import HomePage from './Pages/Home';
 
 const App: React.FC = () => {
     return (
       <div>
         <BrowserRouter>
             <Switch>
+              <Route path="/" exact component={ HomePage } />
               <Route path="/login" exact component={ LoginPage } />
             </Switch>
         </BrowserRouter>
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,81 @@
+import React  from 'react';
+// import React,  { useEffect, useState }  from 'react';
+import { Redirect } from 'react-router-dom';
+import axios from 'axios';
+import { Button } from 'antd';
+import './style.css';
+
+
+// interface State {
+//     isLogin: boolean
+// }
+// 范型里面可以包含另外一个范型，这样就可以使得 props 有语法提示
+// class Home extends React.PureComponent<{}, State> {
+
+
+class Home extends React.PureComponent {
+    // 同样可以把 state 定义在这里，props 也可以得到类型提示
+    state = {
+        loaded: false,
+        isLogin : true
+    };
+    constructor(props: {}) {
+        super(props);
+    }
+    componentDidMount() {
+        this.checkLoginStatus('/api/isLogin');
+    }
+
+    checkLoginStatus = async (url: string) =>  {
+        const res = await axios.get(url);
+        if (!res.data?.data) {
+            this.setState({
+                isLogin : false,
+                loaded: true
+            })
+        }
+    }
+   render() {
+       const { isLogin, loaded} = this.state;
+       if (isLogin && loaded) {
+        return (
+            <div className="home-page">
+              <Button type="primary" >Crawl</Button>
+              <Button type="primary">Display</Button>
+              <Button type="primary">Exit</Button>
+            </div>
+          )
+       }
+       return <Redirect to="/login"/>
+   } 
+}
+
+// Hook 版本： 
+// 返回类型是 React.FC 也可以
+// const Home1: () => JSX.Element = () => {
+
+//     const [isLogin, setLogin] = useState(false);
+    
+//     async function checkLoginStatus(url: string) {
+//         const res = await axios.get(url);
+//         if (res.data?.data) {
+//             setLogin(true);
+//             console.log('Login Already')
+//         } else {
+//             setLogin(false);
+//             console.log('No Login')
+//         }
+//     }
+//     useEffect(() => {
+//         checkLoginStatus('/api/isLogin');
+//     })
+//     return (
+//         <div className="home-page">
+//             <Button type="primary" >Crawl</Button>
+//             <Button type="primary">Display</Button>
+//             <Button type="primary">Exit</Button>
+//         </div>
+//     )
+// }
+
+export default Home;
