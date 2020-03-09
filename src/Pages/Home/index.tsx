f614441f49e828bd4a486de0748eb63187fdbb10
zichenma <zma@liaisionedu.com>
2020-03-09 09:12:18 -0400

    finished 8_6

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,7 +2,8 @@ import React  from 'react';
 // import React,  { useEffect, useState }  from 'react';
 import { Redirect } from 'react-router-dom';
 import axios from 'axios';
-import { Button } from 'antd';
+import ReactEcharts from 'echarts-for-react';
+import { Button, message } from 'antd';
 import './style.css';
 
 
@@ -16,7 +17,6 @@ import './style.css';
 class Home extends React.PureComponent {
     // 同样可以把 state 定义在这里，props 也可以得到类型提示
     state = {
-        // loaded: false,
         isLogin : true
     };
     constructor(props: {}) {
@@ -33,9 +33,6 @@ class Home extends React.PureComponent {
                 isLogin : false,
             })
         }
-        // this.setState({
-        //     loaded: true
-        // })
     }
 
     handleLogoutClick = async () => {
@@ -45,20 +42,93 @@ class Home extends React.PureComponent {
                 isLogin : false,
             })
         }
-        // this.setState({
-        //     loaded: true
-        // })
+    }
+
+    handleCrawllerClick = async () => {
+        const res = await axios.get('/api/getData');
+        if (res.data?.data) {
+            message.success('Crawl Successfully!')
+        } else {
+            message.error('Crawl Failed!')
+        }
+    }
+    
+    // get from echarts definition file
+    getOptions:() => echarts.EChartOption =  () => {
+     return {
+            title: {
+                text: '折线图堆叠'
+            },
+            tooltip: {
+                trigger: 'axis'
+            },
+            legend: {
+                data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            toolbox: {
+                feature: {
+                    saveAsImage: {}
+                }
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [
+                {
+                    name: '邮件营销',
+                    type: 'line',
+                    stack: '总量',
+                    data: [120, 132, 101, 134, 90, 230, 210]
+                },
+                {
+                    name: '联盟广告',
+                    type: 'line',
+                    stack: '总量',
+                    data: [220, 182, 191, 234, 290, 330, 310]
+                },
+                {
+                    name: '视频广告',
+                    type: 'line',
+                    stack: '总量',
+                    data: [150, 232, 201, 154, 190, 330, 410]
+                },
+                {
+                    name: '直接访问',
+                    type: 'line',
+                    stack: '总量',
+                    data: [320, 332, 301, 334, 390, 330, 320]
+                },
+                {
+                    name: '搜索引擎',
+                    type: 'line',
+                    stack: '总量',
+                    data: [820, 932, 901, 934, 1290, 1330, 1320]
+                }
+            ]
+        };        
     }
 
    render() {
-       // const { isLogin, loaded} = this.state;
        const { isLogin } = this.state;
        if (isLogin) {
         return (
             <div className="home-page">
-              <Button type="primary" >Crawl</Button>
-              <Button type="primary">Display</Button>
-              <Button type="primary" onClick={this.handleLogoutClick}>Exit</Button>
+              <div className="buttons">
+                <Button type="primary" onClick={this.handleCrawllerClick}>Crawl</Button>
+                <Button type="primary" onClick={this.handleLogoutClick}>Exit</Button>
+              </div>
+              <ReactEcharts option={this.getOptions()} />
             </div>
           )
        }
